f7941d6cd67e224a1c7cfcccabf1e68c30e0c359
William Tao <tao.w@etoncollege.org.uk>
2022-01-24 11:01:14 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Tao, Weizhe (Coll) on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
